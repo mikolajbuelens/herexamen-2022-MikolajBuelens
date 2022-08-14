1edcd452d6b8a89f344f805fbb41739392c8d14f
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2022-08-14 23:08:31 +0200

    chart fix, hide/show adjustment

--- js/index.js
+++ js/index.js
@@ -44,8 +44,9 @@ class Measurements {
   }
 
   get date() {
-     return new Date(this._timestamp * 1000).toLocaleDateString("nl-BE").split('/');
-    
+    return new Date(this._timestamp * 1000)
+      .toLocaleDateString("nl-BE")
+      .split("/");
   }
 
   get htmlstring() {
@@ -53,7 +54,7 @@ class Measurements {
       <tr>
       <td>${this.unit}</td>
       <td>${this.value}</td>
-      <td>${this.date.join('/')} ${this.time}</td>
+      <td>${this.date.join("/")} ${this.time}</td>
       </tr>`;
   }
 }
@@ -68,77 +69,6 @@ const app = {
     this.fetchData();
     this.filter();
     this.render();
-
-    setTimeout(() => {
-      const arr1 = [];
-      const arr2 = [];
-      const arr3 = [];
-      const arr4 = [];
-      this.renderChart("CO2", arr1);
-      this.renderChart("VOC", arr2);
-      this.renderChart("PM25", arr3);
-      this.renderChart("PM10", arr4);
-      console.log(arr1);
-
-      const data = {
-        labels: [
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ],
-        datasets: [
-          {
-            //? [june: value, july:value,.....]
-            label: "CO2",
-            data: arr1,
-            borderWidth: 1,
-            backgroundColor: "rgba(0, 127, 255, 0.3)",
-            borderColor: "rgba(0, 127, 255, 1)",
-          },
-          {
-            label: "VOC",
-            data: arr2,
-            borderWidth: 1,
-            backgroundColor: "rgba(221, 5, 49, 0.3)",
-            borderColor: "rgba(221, 5, 49, 1)",
-          },
-          {
-            label: "PM25",
-            data: arr3,
-            borderWidth: 1,
-            backgroundColor: "rgba(249, 230, 79, 0.3)",
-            borderColor: "rgba(249, 230, 79, 1)",
-          },
-          {
-            label: "PM10",
-            data: arr4,
-            borderWidth: 1,
-            backgroundColor: "rgba(25, 227, 89, 0.3)",
-            borderColor: "rgba(25, 227, 89, 1)",
-          },
-        ],
-      };
-      const config = {
-        type: "bar",
-        data,
-
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      };
-
-      const myChart = new Chart(document.getElementById("chart"), config);
-
-      // console.log(this.measurements);
-    }, 1000);
   },
 
   fetchData() {
@@ -155,76 +85,109 @@ const app = {
 
   // Apply filter = make new render of chart & table
   filter() {
+    if (this.selectedMeasurement === "all") {
+      this.filtered = this.measurements;
+    } else {
+      this.filtered = this.measurements.filter(
+        (x) => x.unit === this.selectedMeasurement
+      );
+    }
+    this.render();
+    this.selectedMeasurement = document.getElementById("typeFilter").value;
+    // this.renderChart("CO2");
+
     setTimeout(() => {
-      if (this.selectedMeasurement === "all") {
-        this.filtered = this.measurements;
+      if (
+        this.selectedMeasurement === "all" ||
+        this.selectedMeasurement === "CO2"
+      ) {
+        myChart.setDatasetVisibility(0, true);
       } else {
-        this.filtered = this.measurements.filter(
-          (x) => x.unit === this.selectedMeasurement
-        );
+        myChart.setDatasetVisibility(0, false);
       }
-      this.render();
-      this.selectedMeasurement = document.getElementById("typeFilter").value;
-      // this.renderChart("CO2");
-    }, 50);
+      if (
+        this.selectedMeasurement === "all" ||
+        this.selectedMeasurement === "VOC"
+      ) {
+        myChart.setDatasetVisibility(1, true);
+      } else {
+        myChart.setDatasetVisibility(1, false);
+      }
+      if (
+        this.selectedMeasurement === "all" ||
+        this.selectedMeasurement === "PM25"
+      ) {
+        myChart.setDatasetVisibility(2, true);
+      } else {
+        myChart.setDatasetVisibility(2, false);
+      }
+      if (
+        this.selectedMeasurement === "all" ||
+        this.selectedMeasurement === "PM10"
+      ) {
+        myChart.setDatasetVisibility(3, true);
+      } else {
+        myChart.setDatasetVisibility(3, false);
+      }
+      myChart.update();
+    }, 100);
   },
   //render chart (Library)
   renderChart(type, arr) {
-
-    
-      
-    
     // let splitter = this.measurements.map((m) => {
     //   m.date = m.date.split("/");
     //   return m;
     // });
     // console.log(splitter);
-  
+    setTimeout(() => {
+      for (let i = 5; i < 12; ) {
+        i++;
+        i = i.toString();
 
-    for (let i = 5; i < 12; ) {
-      i++;
-      i = i.toString();
+        //! need to fix split
+        // let split = this.measurements.map((m) => m.date.split("/"));
+        // let months = split.filter(m => m[1] === i);
+        // console.log(months);
 
-      //! need to fix split
-      // let split = this.measurements.map((m) => m.date.split("/"));
-      // let months = split.filter(m => m[1] === i);
-      // console.log(months);
+        // console.log(split);
 
-      // console.log(split);
+        // console.warn(this.measurements);
 
-      // console.warn(this.measurements);
+        // let date = "01/11/2016";
 
-      // let date = "01/11/2016";
+        // var arr2 = date.split("/");
 
-      // var arr2 = date.split("/");
+        // console.log(arr2[1]);
 
-      // console.log(arr2[1]);
+        // let testSplit = this.measurements.forEach((s) => s.date.split("/"));
+        // let monthTest =  testSplit.filter((y) =>console.log( y.date[1]) === i);
+        // console.log(testSplit);
+        // console.log(monthTest);
 
-      // let testSplit = this.measurements.forEach((s) => s.date.split("/"));
-      // let monthTest =  testSplit.filter((y) =>console.log( y.date[1]) === i);
-      // console.log(testSplit);
-      // console.log(monthTest);
+        // let split = this.date.split;
 
-      // let split = this.date.split;
+        // let splitter = this.measurements.map((mapped) => mapped.date.split("/"));
+        // let month = splitter.filter((m) => m[1] === i);
+        // console.log(month);
+        let testMonth = this.measurements.filter((f) => f.date[1] === i);
 
-      // let splitter = this.measurements.map((mapped) => mapped.date.split("/"));
-      // let month = splitter.filter((m) => m[1] === i);
-      // console.log(month);
-      let testMonth = this.measurements.filter((f) => f.date[1] === i);
-
-      // let month = testMonth.filter((f) => f.date[1] === i);
-      let unit = testMonth.filter((u) => u.unit === type);
-      // console.log(unit);
-      let reduced = unit.reduce((a, b) => {
-        return a + b.value;
-      }, 0);
-      // console.log(reduced);
-      arr.push(reduced);
-      // console.log(reduced);
-    }
+        // let month = testMonth.filter((f) => f.date[1] === i);
+        let unit = testMonth.filter((u) => u.unit === type);
+        // console.log(unit);
+        let reduced = unit.reduce((a, b) => {
+          return a + b.value;
+        }, 0);
+        // console.log(reduced);
+        arr.push(reduced);
+        // console.log(reduced);
+      }
+    }, 100);
   },
 
   render() {
+    setTimeout(() => {
+      
+   
     document.getElementById("measurements").innerHTML = ``;
     this.filtered.forEach(
       (x) => (document.getElementById("measurements").innerHTML += x.htmlstring)
@@ -233,42 +196,80 @@ const app = {
       this.selectedMeasurement = document.getElementById("typeFilter").value;
       this.filter();
 
-      console.log(this.selectedMeasurement);
-      //! true = show, hide = false
-      if (this.selectedMeasurement === "all") {
-        myChart.setDatasetVisibility(0, true);
-        myChart.setDatasetVisibility(1, true);
-        myChart.setDatasetVisibility(2, true);
-        myChart.setDatasetVisibility(3, true);
-        myChart.update();
-      } else if (this.selectedMeasurement === "CO2") {
-        myChart.setDatasetVisibility(0, true);
-        myChart.setDatasetVisibility(1, false);
-        myChart.setDatasetVisibility(2, false);
-        myChart.setDatasetVisibility(3, false);
-        myChart.update();
-      } else if (this.selectedMeasurement === "VOC") {
-        myChart.setDatasetVisibility(0, false);
-        myChart.setDatasetVisibility(1, true);
-        myChart.setDatasetVisibility(2, false);
-        myChart.setDatasetVisibility(3, false);
-        myChart.update();
-      } else if (this.selectedMeasurement === "PM25") {
-        myChart.setDatasetVisibility(0, false);
-        myChart.setDatasetVisibility(1, false);
-        myChart.setDatasetVisibility(2, true);
-        myChart.setDatasetVisibility(3, false);
-        myChart.update();
-      } else if (this.selectedMeasurement === "PM10") {
-        myChart.setDatasetVisibility(0, false);
-        myChart.setDatasetVisibility(1, false);
-        myChart.setDatasetVisibility(2, false);
-        myChart.setDatasetVisibility(3, true);
-        myChart.update();
-      }
+      
+      
     };
+  }, 100);
   },
 };
+  const arr1 = [];
+  const arr2 = [];
+  const arr3 = [];
+  const arr4 = [];
+  app.renderChart("CO2", arr1);
+  app.renderChart("VOC", arr2);
+  app.renderChart("PM25", arr3);
+  app.renderChart("PM10", arr4);
+  console.log(arr1);
+
+  const data = {
+    labels: [
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+    datasets: [
+      {
+        //? [june: value, july:value,.....]
+        label: "CO2",
+        data: arr1,
+        borderWidth: 1,
+        backgroundColor: "rgba(0, 127, 255, 0.3)",
+        borderColor: "rgba(0, 127, 255, 1)",
+      },
+      {
+        label: "VOC",
+        data: arr2,
+        borderWidth: 1,
+        backgroundColor: "rgba(221, 5, 49, 0.3)",
+        borderColor: "rgba(221, 5, 49, 1)",
+      },
+      {
+        label: "PM25",
+        data: arr3,
+        borderWidth: 1,
+        backgroundColor: "rgba(249, 230, 79, 0.3)",
+        borderColor: "rgba(249, 230, 79, 1)",
+      },
+      {
+        label: "PM10",
+        data: arr4,
+        borderWidth: 1,
+        backgroundColor: "rgba(25, 227, 89, 0.3)",
+        borderColor: "rgba(25, 227, 89, 1)",
+      },
+    ],
+  };
+  const config = {
+    type: "bar",
+    data,
+
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
+  const myChart = new Chart(document.getElementById("chart"), config);
+
+  // console.log(this.measurements);
 // console.log(app.fetchData);
 // console.log(app);
 
